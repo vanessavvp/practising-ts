f4ecfec94e0bfc061376af39c28554c7a3898f64
Vanessa Villalba <vanessavillalba@VanessaVillalba>
2023-05-01 19:52:55 +0100

    exercise 1 from section 3, done

--- bases/app.ts
+++ bases/app.ts
@@ -1,5 +1,42 @@
 (() => {
-  const a: number = 10;
 
-  console.log(a);
-})();
+    // Tipos
+    const batman: string = 'Bruce';
+    const superman: string = 'Clark';
+  
+    const existe: boolean = false;
+  
+    // Tuplas
+    const parejaHeroes: [string, string] = [batman,superman];
+    const villano: [string, number, boolean] = ['Lex Lutor',5,true];
+  
+    // Arreglos
+    const aliados: string[]= ['Mujer Maravilla','Acuaman','San', 'Flash'];
+  
+    //Enumeraciones
+    enum HerosStrenght {
+      aquaman = 0,
+      batman = 1,
+      flash = 5,
+      superman = 100,
+    }
+ 
+  
+    // Retorno de funciones
+    function activar_batiseñal(): string{
+      return 'activada';
+    }
+  
+    function pedir_ayuda(): void {
+      console.log('Auxilio!!!');
+    }
+  
+    // Aserciones de Tipo
+    const poder: any = '100';
+    const largoDelPoder:number = (poder as string).length;
+    console.log( largoDelPoder );
+  
+  
+  })()
+  
+  
